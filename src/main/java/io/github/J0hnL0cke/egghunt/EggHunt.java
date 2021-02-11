28f0edea20186604e4a79ecaeafe775cc313e3f8
ThroawayUser <78572188+ThroawayUser@users.noreply.github.com>
2021-02-10 23:52:31 -0500

    Update EggHunt.java
    Fixing FileSave System

--- src/main/java/io/github/J0hnL0cke/egghunt/EggHunt.java
+++ src/main/java/io/github/J0hnL0cke/egghunt/EggHunt.java
@@ -4,15 +4,22 @@ import java.util.UUID;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.CompassMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import io.github.J0hnL0cke.egghunt.EggHuntListener.Egg_Storage_Type;
 
-public final class EggHunt extends JavaPlugin {
+
+public final class egghunt extends JavaPlugin {
+
+
+	FileSave config = new FileSave(this);
 
 	@Override
 	public void onEnable() {
@@ -20,7 +27,10 @@ public final class EggHunt extends JavaPlugin {
 		getLogger().info("onEnable has been invoked, registering event listeners.");
 		//register event handlers
 		getServer().getPluginManager().registerEvents(new EggHuntListener(getLogger()), this);
-		//TODO: load in save files
+		//load saved data
+		getLogger().info("Loading save data.");
+		loadData();
+		//done
 		getLogger().info("Done!");
 	}
 
@@ -28,6 +38,62 @@ public final class EggHunt extends JavaPlugin {
 	public void onDisable() {
 		// TODO Insert logic to be performed when the plugin is disabled
 		getLogger().info("onDisable has been invoked.");
+	}
+
+	public void loadData() {
+		//load loc
+
+		if (config.keyExists("loc")) {
+			String[] loc=config.getKey("loc", "").split(":");
+			if (loc.length==4) {
+				World w = Bukkit.getServer().getWorld(loc[0]);
+				double x = Double.parseDouble(loc[1]);
+				double y = Double.parseDouble(loc[2]);
+				double z = Double.parseDouble(loc[3]);
+				EggHuntListener.loc=new Location(w,x,y,z);
+			}
+		}
+		//load stored_entity
+		if (config.keyExists("stored_entity")) {
+			UUID id=UUID.fromString(config.getKey("stored_entity", null));
+			boolean found=false;
+
+			for (World world : Bukkit.getWorlds()) {
+				if (!found) {
+					for (Entity entity:world.getEntities()) {
+
+						if (entity.getUniqueId()==id) {
+							EggHuntListener.stored_entity=entity;
+							found=true;
+							break;
+						}
+					}
+				}
+			}
+		}
+		//load stored_as
+		if (config.keyExists("stored_as")){
+			EggHuntListener.stored_as=Egg_Storage_Type.valueOf(config.getKey("stored_as", null));
+		}
+		//load owner
+		if (config.keyExists("owner")) {
+			EggHuntListener.owner=UUID.fromString(config.getKey("owner", null));
+		}
+	}
+
+	public void saveData() {
+		//save loc
+		Location loc=EggHuntListener.loc;
+		config.writeKey("loc", loc.getWorld().getName() + ":" + loc.getBlockX() + ":" + loc.getBlockY() + ":" + loc.getBlockZ());
+
+		//save stored_entity
+		config.writeKey("stored_entity", EggHuntListener.stored_entity.getUniqueId().toString());
+
+		//save stored_entity
+		config.writeKey("stored_as", EggHuntListener.stored_as.name());
+
+		//save owner
+		config.writeKey("owner", EggHuntListener.owner.toString());
 	}
 
 	public Location getEggLocation() {
