473f3878665e2e016ed189f3f8c409941288fbc9
J0nasL <51202569+J0hnL0cke@users.noreply.github.com>
2023-07-30 11:52:51 -0400

    Fixed version check
    Now compatible with 1.17+

--- src/main/java/io/github/J0hnL0cke/egghunt/Model/Version.java
+++ src/main/java/io/github/J0hnL0cke/egghunt/Model/Version.java
@@ -12,10 +12,14 @@ public class Version {
     private boolean cavesAndCliffsPart1;
     private boolean netherUpdate;
 
-    public Version(){
-        String version = Bukkit.getVersion();
-        majorVersion = Integer.parseInt(version.split(".")[1]);
-
+    public Version() {
+        //Convert raw version string to major game version
+        String version = Bukkit.getVersion(); //git-Paper-411 (MC: 1.17.1)
+        version = version.substring(version.indexOf(":")); //: 1.17.1)
+        String[] v = version.split("\\."); //[": 1","17","1)"] split at periods, use \\ to escape period in regex
+        version = v[1]; //17
+        majorVersion = Integer.parseInt(version);
+        
         cavesAndCliffsPart1 = majorVersion >= 17;
         netherUpdate = majorVersion >= 16;
     }
